3b946d078690a5a064ff87b449164efdc5f1645a
Jamie Phillips <cphill11@gmail.com>
2019-06-11 21:22:10 -0400

    Following a bash convention.

--- bash/build.sh
+++ bash/build.sh
@@ -1,112 +1,112 @@
 #! /bin/bash
 
 # Initial Setup
-RESOURCE_GROUP_NAME="grafana-eus-rg"
-LOCATION="eastus"
-STORAGE_ACCOUNT_NAME="grafanaeusst"
-APP_PLAN_NAME="grafana-eus-ap"
-APP_SERVICE_NAME="grafana-eus-as"
+resource_group_name="grafana-eus-rg"
+location="eastus"
+storage_account_name="grafanaeusst"
+app_plan_name="grafana-eus-ap"
+app_service_name="grafana-eus-as"
 
 # Generating a Grafana password
-GF_PASSWORD=$(date +%s | sha256sum | base64 | head -c 12 ;)
+gf_password=$(date +%s | sha256sum | base64 | head -c 12 ;)
 
 # Getting tenant id for use later
-TENANT_ID=$(az account show --query 'tenantId' --output tsv)
+tenant_id=$(az account show --query 'tenantId' --output tsv)
 
 # Create Resource Group
 az group create \
-    --name $RESOURCE_GROUP_NAME \
-    --location $LOCATION \
+    --name $resource_group_name \
+    --location $location \
     --output none
 
 # Create Storage Account
 az storage account create \
-    --name $STORAGE_ACCOUNT_NAME \
-    --resource-group $RESOURCE_GROUP_NAME \
-    --location $LOCATION \
+    --name $storage_account_name \
+    --resource-group $resource_group_name \
+    --location $location \
     --sku Standard_LRS \
     --output none
 
 # Get Storage Account Key
-STORAGE_ACCOUNT_KEY=$(az storage account keys list \
-    --account-name $STORAGE_ACCOUNT_NAME \
-    --resource-group $RESOURCE_GROUP_NAME \
+storage_account_key=$(az storage account keys list \
+    --account-name $storage_account_name \
+    --resource-group $resource_group_name \
     --query '[0].value' \
     --output tsv)
 
 # Create Grafana Container
 az storage container create \
     --name grafana \
-    --account-name $STORAGE_ACCOUNT_NAME 
+    --account-name $storage_account_name 
 
 # Create App Service Plan
 az appservice plan create \
-    --name $APP_PLAN_NAME \
-    --resource-group $RESOURCE_GROUP_NAME \
+    --name $app_plan_name \
+    --resource-group $resource_group_name \
     --sku B1 \
     --is-linux \
     --output none
 
 # Create App Service
 az webapp create \
-    --resource-group $RESOURCE_GROUP_NAME \
-    --plan $APP_PLAN_NAME \
-    --name $APP_SERVICE_NAME \
+    --resource-group $resource_group_name \
+    --plan $app_plan_name \
+    --name $app_service_name \
     --deployment-container-image-name grafana/grafana \
     --output none
 
 # Set the storage account and mount point
 az webapp config storage-account add \
-    --resource-group $RESOURCE_GROUP_NAME \
-    --name $APP_SERVICE_NAME \
+    --resource-group $resource_group_name \
+    --name $app_service_name \
     --custom-id GrafanaData \
     --storage-type AzureBlob \
     --share-name grafana \
-    --account-name $STORAGE_ACCOUNT_NAME \
-    --access-key $STORAGE_ACCOUNT_KEY \
+    --account-name $storage_account_name \
+    --access-key $storage_account_key \
     --mount-path /var/lib/grafana/ \
     --output none
 
 # Get the hostname
-HOSTNAME=$(az webapp show \
-    --name $APP_SERVICE_NAME \
-    --resource-group $RESOURCE_GROUP_NAME \
-    --query 'defaultHostName' \
+hostname=$(az webapp show \
+    --name $app_service_name \
+    --resource-group $resource_group_name \
+    --query 'defaulthostname' \
     --output tsv)
 
-CLIENT_SECRET=$(uuidgen)
+client_secret=$(uuidgen)
 
 # App Registration
 # https://grafana.com/docs/auth/generic-oauth/#set-up-oauth2-with-azure-active-directory
-APP_ID=$(az ad app create \
+application_id=$(az ad app create \
     --display-name Grafana \
-    --reply-urls https://$HOSTNAME/login/generic_oauth \
-    --password $CLIENT_SECRET \
+    --reply-urls https://$hostname/login/generic_oauth \
+    --password $client_secret \
     --query 'appId' \
     --output tsv)
 
 # Configuring the settings that will become environment variables
 az webapp config appsettings set \
-    --resource-group $RESOURCE_GROUP_NAME \
-    --name $APP_SERVICE_NAME \
+    --resource-group $resource_group_name \
+    --name $app_service_name \
     --settings \
-    GF_SERVER_ROOT_URL=https://$HOSTNAME \
-    GF_SECURITY_ADMIN_PASSWORD=$GF_PASSWORD \
+    GF_SERVER_ROOT_URL=https://$hostname \
+    GF_SECURITY_ADMIN_PASSWORD=$gf_password \
     GF_INSTALL_PLUGINS=grafana-clock-panel,grafana-simple-json-datasource,grafana-azure-monitor-datasource \
     GF_AUTH_GENERIC_OAUTH_NAME="Azure AD" \
     GF_AUTH_GENERIC_OAUTH_ENABLED=true \
     GF_AUTH_GENERIC_OAUTH_ALLOW_SIGN_UP=true \
-    GF_AUTH_GENERIC_OAUTH_CLIENT_ID=$APP_ID \
-    GF_AUTH_GENERIC_OAUTH_CLIENT_SECRET=$CLIENT_SECRET \
+    GF_AUTH_GENERIC_OAUTH_CLIENT_ID=$application_id \
+    GF_AUTH_GENERIC_OAUTH_client_secret=$client_secret \
     GF_AUTH_GENERIC_OAUTH_SCOPES="openid email name" \
-    GF_AUTH_GENERIC_OAUTH_AUTH_URL=https://login.microsoftonline.com/$TENANT_ID/oauth2/authorize \
-    GF_AUTH_GENERIC_OAUTH_TOKEN_URL=https://login.microsoftonline.com/$TENANT_ID/oauth2/token \
+    GF_AUTH_GENERIC_OAUTH_AUTH_URL=https://login.microsoftonline.com/$tenant_id/oauth2/authorize \
+    GF_AUTH_GENERIC_OAUTH_TOKEN_URL=https://login.microsoftonline.com/$tenant_id/oauth2/token \
     GF_AUTH_GENERIC_OAUTH_API_URL="" \
     GF_AUTH_GENERIC_OAUTH_TEAM_IDS="" \
     GF_AUTH_GENERIC_OAUTH_ALLOWED_ORGANIZATIONS="" \
     --output none
 
 # Printing out information you will need to know
-echo Grafana password is: $GF_PASSWORD
-echo Grafana address is: https://$HOSTNAME
-echo Client Scecret is: $CLIENT_SECRET
+echo Grafana password is: $gf_password
+echo Grafana address is: https://$hostname
+echo Client Scecret is: $client_secret
